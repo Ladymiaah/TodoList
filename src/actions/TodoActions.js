4205af9c4395daebabf71d4d41fe61bd19708481
Adedoyin Lawal <adedoyinlawal819@gmail.com>
2023-03-28 11:21:03 +0100

    second commit

---
+++ src/actions/TodoActions.js
@@ -0,0 +1,28 @@
+export const AddTodoAction = (todo) => (dispatch, getState) => {
+    const {
+        Todo : {todos},
+    } = getState();
+
+    const hasTodo = todos.find((i)=> i.todo === todo);
+
+    if (!hasTodo && todo !== "") {
+        dispatch({
+            type: "ADD_TODO_SUCCESS",
+            payload: [{ id:todo, todo}, ...todos],
+        });
+    }
+};
+
+export const RemoveTodoAction = (todo) => (dispatch, getState) =>{
+    const {
+        Todo : {todos},
+    } = getState();
+
+    dispatch({
+        type: "REMOVETODO",
+        payload: todos.filter((t) => t.id !==todo.id),
+    });
+};
+
+
+
